4e6309dfcff33585ef0f3529662e9aa3af148645
Fogoid <diogo.barradas.fernandes@protonmail.com>
2024-07-03 15:55:59 +0100

    Cleanup neovim config

--- nvim/lua/fogoid/plugins/essentials.lua
+++ nvim/lua/fogoid/plugins/essentials.lua
@@ -3,9 +3,9 @@ return {
     {
         'nvim-telescope/telescope.nvim',
         dependencies = {
-           'nvim-lua/plenary.nvim'
+            'nvim-lua/plenary.nvim'
         },
-        init =  function ()
+        init = function()
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
             vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
@@ -16,46 +16,39 @@ return {
     },
     {
         'nvim-treesitter/nvim-treesitter',
-        config =  function ()
+        config = function()
             require('nvim-treesitter.configs').setup {
-              ensure_installed = { "c", "python", "lua", "vim", "vimdoc", "query", "rust", "c_sharp", "java" },
+                ensure_installed = { "c", "python", "lua", "vim", "vimdoc", "query", "rust", "c_sharp", "java" },
 
-              -- Install parsers synchronously (only applied to `ensure_installed`)
-              sync_install = false,
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
 
-              -- Automatically install missing parsers when entering buffer
-              -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-              auto_install = true,
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+                auto_install = true,
 
-              highlight = {
-                enable = true,
+                highlight = {
+                    enable = true,
 
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-              },
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
+                },
             }
         end
     },
     {
-        'mbbill/undotree',
-        init = function()
-            vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-        end
-    },
-
-    {
         'ThePrimeagen/harpoon',
         branch = 'harpoon2',
         dependencies = {
-           'plenary'
+            'plenary'
         },
-        config = function ()
+        config = function()
             require('harpoon'):setup()
         end,
-        init = function ()
+        init = function()
             local harpoon = require('harpoon')
             vim.keymap.set('n', '<leader>wn', function() harpoon:list():next() end)
             vim.keymap.set('n', '<leader>wp', function() harpoon:list():prev() end)
