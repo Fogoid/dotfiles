0d340daf57211eba115e1a1ed289f8d5529e445f
Fogoid <diogo.barradas.fernandes@protonmail.com>
2025-03-31 22:27:38 +0100

    change db access plugin

--- nvim/lua/fogoid/plugins/db.lua
+++ nvim/lua/fogoid/plugins/db.lua
@@ -1,37 +1,19 @@
 return {
-    {
-      "kndndrj/nvim-dbee",
-      dependencies = {
-        "MunifTanjim/nui.nvim",
-      },
-      build = function()
-        -- Install tries to automatically detect the install method.
-        -- if it fails, try calling it with one of these parameters:
-        --    "curl", "wget", "bitsadmin", "go"
-        require("dbee").install()
-      end,
-      config = function()
-        require("dbee").setup({
-            sources = {
-                require("dbee.sources").MemorySource:new({
-                    {
-                        name = "egrc_diogofernandes",
-                        url = "developer:@tcp(10.101.2.100)/egrc_diogofernandes",
-                        type = "mysql",
-                    },
-                    {
-                        name = "eapi_diogofernandes",
-                        url = "developer:@tcp(10.101.2.100)/eapi_diogofernandes",
-                        type = "mysql",
-                    },
-                }),
-            },
-        })
-      end,
-      keys = {
-          { "<leader>dbo", function() require("dbee").open() end },
-          { "<leader>dbc", function() require("dbee").close() end },
-          { "<leader>dbt", function() require("dbee").toggle() end },
-      },
-    },
+  'kristijanhusak/vim-dadbod-ui',
+  dependencies = {
+    { 'tpope/vim-dadbod', lazy = true },
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+  },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.dbs = {
+    }
+  end,
 }
